1c6ff51990fd0be46dd0c62942df696da690794b
huannx <huannx.dev@gmail.com>
2020-10-21 23:45:22 +0700

    deploy heroku

---
+++ server.js
@@ -0,0 +1,5 @@
+app.use(express.static('./dist/ng-select-dropdown'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/ng-select-dropdown/'}),
+);
+app.listen(process.env.PORT || 8080);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { NgSelectDropdownModule } from 'dist/ng-select-dropdown';
+import { NgSelectDropdownModule } from '../../projects/ng-select-dropdown/src/lib/ng-select-dropdown.module';
 
 @NgModule({
   declarations: [
